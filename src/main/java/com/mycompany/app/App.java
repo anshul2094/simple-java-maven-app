67c49f3fcf5ea91610158d61a3884f74136dc62f
root <root@java-maven-jenkins.us-central1-a.c.seismic-harmony-268206.internal>
2020-09-20 15:55:27 +0000

    Changes made successfully. Congratulations Anshul

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Whats up!!!!!!!!!";
+    private final String message = "Hello World! Welcome to Infosys: PUNE!! Enjoy your Stay!!!!!!!";
 
     public App() {}
 
